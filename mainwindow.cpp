601051d8755565c3cc40ae9cd05d1a4ea500e6ff
Gildson <37073818+Gildson@users.noreply.github.com>
2022-01-20 22:12:13 -0300

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <iostream>
+#include <stdlib.h>
+#include <signal.h>
+#include <stdio.h>
+#include <QLineEdit>
+#include <QWidget>
+
+using namespace std;
+
+QString pid;
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +24,31 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_pushButton_Kill_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_2_Stop_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_3_Cont_clicked()
+{
+
+}
+void MainWindow::filtro(){
+    QString textoFiltro = ui->lineEdit_4_Filtro->text();
+
+    QString comando;
+    comando = " ps -auf | grep " + textoFiltro;
+
+    QByteArray comandoConvertido = comando.toLocal8Bit();
+    const char *comandoFinal = comandoConvertido.data();
+
+    system(comandoFinal);
+}
+void MainWindow::pegarPID(){
+    pid = ui->lineEdit_PID->text();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLineEdit>
+#include <QWidget>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +16,17 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_pushButton_Kill_clicked();
+
+    void on_pushButton_2_Stop_clicked();
+
+    void on_pushButton_3_Cont_clicked();
+
+    void filtro();
+
+    void pegarPID();
 
 private:
     Ui::MainWindow *ui;
